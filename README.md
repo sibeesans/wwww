c0409d2d176c6363a738ed1982904e62cab4c8f0
thoiruddin <47009252+thoiruddin@users.noreply.github.com>
2023-06-30 00:46:16 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,65 @@
+# DXVPN AIO
+- `Note !!! for multipath please change to Xray-core mod in menu script`
+
+# UPDATES LOG
+
+- `ADD DNS SETTING`
+
+# Required
+- DOMAIN (MUST)
+- DEBIAN 9/10
+- Ubuntu 18/20 LTS
+- CPU MIN 1 CORE
+- RAM 1GB
+
+|  SERVICE  |  NETWORK PORT  |
+|---------- |--------|
+| Vmess WS TLS (multipath)  | 443 |
+| Vless WS TLS  | 443 |
+| Trojan WS TLS  | 443 |
+| Socks5 WS TLS  | 443 |
+| Shadowsocks WS TLS (aes-256-gcm)  | 443 |
+| Shadowsocks 2022 WS TLS (2022-blake3-aes-256-gcm)  | 443 |
+| Vmess WS (multipath)  | 80 |
+| Vless WS  | 80 |
+| Trojan WS  | 80 |
+| Socks5 WS  | 80 |
+| Shadowsocks WS (aes-256-gcm)  | 80 |
+| Shadowsocks 2022 WS (2022-blake3-aes-256-gcm)  | 80 |
+| Vmess gRPC  | 443 |
+| Vless gRPC  | 443 |
+| Trojan gRPC  | 443 |
+| Socks5 gRPC  | 443 |
+| Shadowsocks gRPC (aes-256-gcm)  | 443 |
+| Shadowsocks 2022 gRPC (2022-blake3-aes-256-gcm)  | 443 |
+| Nginx Webserver | 8000 |
+| Auto Delete Expired Account | ✅ |
+| DNS Setting | ✅ |
+
+|  ALTERNATIF PORT  |  NETWORK PORT  |
+|-------------------|--------|
+| HTTPS  | 2053, 2083, 2087, 2096, 8443 |
+| HTTP  | 8080, 8880, 2052, 2082, 2086, 2095 |
+
+# Setup DNS Cloudflare
+
+|  RECORD TYPE | TTL   |
+|-------------------|--------|
+| A = DNS ONLY  | AUTO TTL |
+| CNAME = DNS ONLY | AUTO TTL |
+
+|  EXAMPLE |
+|-------------------|--------|
+| A  | AUTO TTL |test.yourdomain.tld |
+| CNAME  | AUTO TTL | *.test.yourdomain.tld |
+
+
+# Installation
+- via WGET
+```
+bash -c "$(wget -qO- https://raw.githubusercontent.com/thoiruddin/DXVPN/master/xray)"
+```
+- via CURL
+```
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/thoiruddin/DXVPN/master/xray)"
+```
---
+++ menu/menu.sh
@@ -0,0 +1,99 @@
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+xray_service=$(systemctl status xray | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+nginx_service=$(systemctl status nginx | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+if [[ $xray_service == "running" ]]; then
+status_xray="${GB}[ ON ]${NC}"
+else
+status_xray="${RB}[ OFF ]${NC}"
+fi
+if [[ $nginx_service == "running" ]]; then
+status_nginx="${GB}[ ON ]${NC}"
+else
+status_nginx="${RB}[ OFF ]${NC}"
+fi
+dtoday="$(vnstat | grep today | awk '{print $2" "substr ($3, 1, 3)}')"
+utoday="$(vnstat | grep today | awk '{print $5" "substr ($6, 1, 3)}')"
+ttoday="$(vnstat | grep today | awk '{print $8" "substr ($9, 1, 3)}')"
+dmon="$(vnstat -m | grep `date +%G-%m` | awk '{print $2" "substr ($3, 1 ,3)}')"
+umon="$(vnstat -m | grep `date +%G-%m` | awk '{print $5" "substr ($6, 1 ,3)}')"
+tmon="$(vnstat -m | grep `date +%G-%m` | awk '{print $8" "substr ($9, 1 ,3)}')"
+domain=$(cat /usr/local/etc/xray/domain)
+ISP=$(cat /usr/local/etc/xray/org)
+CITY=$(cat /usr/local/etc/xray/city)
+WKT=$(cat /usr/local/etc/xray/timezone)
+DATE=$(date -R | cut -d " " -f -4)
+MYIP=$(curl -sS ipv4.icanhazip.com)
+clear
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "             ${GB}----- [ DXVPN AIO Script ] -----${NC}           "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e " ${CB}Public IP${NC}        ${GB}: $MYIP"
+echo -e " ${CB}Service Provider${NC} ${GB}: $ISP"
+echo -e " ${CB}Timezone${NC}         ${GB}: $WKT${NC}"
+echo -e " ${CB}City${NC}             ${GB}: $CITY${NC}"
+echo -e " ${CB}Date${NC}             ${GB}: $DATE${NC}"
+echo -e " ${CB}Domain${NC}           ${GB}: $domain${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "     ${GB}NGINX STATUS :${NC} $status_nginx    ${GB}XRAY STATUS :${NC} $status_xray   "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "          ${GB}----- [ Bandwidth Monitoring ] -----${NC}"
+echo -e ""
+echo -e "  ${GB}Today ($DATE)     Monthly ($(date +%B/%Y))${NC}      "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "    ${CB}↓↓ Down: $dtoday          ↓↓ Down: $dmon${NC}   "
+echo -e "    ${CB}↑↑ Up  : $utoday          ↑↑ Up  : $umon${NC}   "
+echo -e "    ${CB}≈ Total: $ttoday          ≈ Total: $tmon${NC}   "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                ${GB}----- [ DXVPN Menu ] -----${NC}              "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e " ${MB}[1]${NC} ${YB}Vmess Menu${NC}          ${MB}[5]${NC} ${YB}Shadowsocks 2022 Menu${NC}"
+echo -e " ${MB}[2]${NC} ${YB}Vless Menu${NC}          ${MB}[6]${NC} ${YB}Socks5 Menu${NC}"
+echo -e " ${MB}[3]${NC} ${YB}Trojan Menu${NC}         ${MB}[7]${NC} ${YB}All Xray Menu${NC}"
+echo -e " ${MB}[4]${NC} ${YB}Shadowsocks Menu${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                 ${GB}----- [ Utility ] -----${NC}                "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e " ${MB}[8]${NC} ${YB}Log Create Account${NC}  ${MB}[13]${NC} ${YB}DNS Setting${NC}"
+echo -e " ${MB}[9]${NC} ${YB}Speedtest${NC}           ${MB}[14]${NC} ${YB}Check DNS Status${NC}"
+echo -e " ${MB}[10]${NC} ${YB}Change Domain${NC}      ${MB}[15]${NC} ${YB}Change Xray-core Mod${NC}"
+echo -e " ${MB}[11]${NC} ${YB}Renew Cert${NC}         ${MB}[16]${NC} ${YB}Change Xray-core Official${NC}"
+echo -e " ${MB}[12]${NC} ${YB}About Script${NC}       ${MB}[x]${NC} ${YB}Exit Menu${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e ""
+read -p " Select Menu :  "  opt
+echo -e ""
+case $opt in
+1) clear ; vmess ;;
+2) clear ; vless ;;
+3) clear ; trojan ;;
+4) clear ; shadowsocks ;;
+5) clear ; shadowsocks2022 ;;
+6) clear ; socks ;;
+7) clear ; allxray ;;
+8) clear ; log-create ;;
+9) clear ; speedtest ;;
+10) clear ; dns ;;
+11) clear ; certxray ;;
+12) clear ; about ;;
+13) clear ; changer ;;
+14) clear ;
+resolvectl status
+echo ""
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+echo ""
+echo ""
+menu ;;
+15) clear ; xraymod ;;
+16) clear ; xrayofficial ;;
+x) clear ; exit ;;
+*) echo -e "${RB}wrong input${NC}" ; sleep 1 ; menu ;;
+esac
